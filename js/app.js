3f557dd60ed9460f78bce7c0592fe933fded57cd
Your Name <you@example.com>
2023-01-06 17:34:02 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+  // let age = 2
+
+  
+
+
+  // if ( age >= 8 && age <=15){
+  //   console.log("Yosh bolasiz hali");
+  // }
+  // else if (age > 15 && age <= 25){
+  //   console.log("Voyaga yetgansiz");
+  // }
+  // else if (age > 25 && age <= 40){
+  //   console.log("O`rta yoshdasiz");
+  // }
+  // else if ( age > 40 && age <=60){
+  //   console.log("Yoshi katta insonsiz");
+  // }
+  // else if(age > 60 && age <=80){
+  //   console.log("Oqsoqolsiz amaki");
+  // }
+  // else if ( age > 80){
+  //   console.log("Umriz uzoq bo`lsin");
+  // }
+  // else{
+  //   console.log("Chaqaloq");
+  // }
+
+
+
+
+  let money = Number(prompt("Senda qancha pul bor?"))
+
+
+  let dolor =  10900
+  let yevro = 12272
+  let count__dolor = 750
+  let count__yevro = 120
+  var sum__dolor = dolor * count__dolor
+  var sum__yevro = yevro * count__yevro
+  let all = sum__dolor + sum__yevro
+  
+  
+  if (money >= all){
+    console.log("Oq yo`l Alisher !");
+  }
+  else{
+    console.log("Alisher ozgina sabr qilish kerak bo`lar ekan!!!");
+  }
